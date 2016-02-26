9c0919a15b412ac42200451f23e0a79d32754dc6
Johannes <johannes.wikner@gmail.com>
2016-02-26 17:54:23 +0100

    Center canvas

--- src/view/view_entry.js
+++ src/view/view_entry.js
@@ -8,6 +8,13 @@ import config from '../config'
 import Scene from './scene'
 
 export default function(sceneData) {
+  document.body.style.backgroundColor = '#000'
+  document.body.style.display = 'flex'
+  document.body.style.justifyContent = 'center'
+  document.body.style.height = '100%'
+  document.body.style.margin = '0'
+  document.body.style.flexDirection = 'column'
+  document.body.parentNode.style.height = '100%'
   const l = PIXI.loader
   Object.keys(sceneData.assets).forEach(
     key => l.add(key, sceneData.assets[key])
