a916f87b9bd34a8796067e5b246d2206c7167e82
Johannes <johannes.wikner@gmail.com>
2016-03-01 19:38:13 +0100

    Add editor
      - Change initial camera pos
      - Change bg scale

---
+++ src/levdev/editor.jsx
@@ -0,0 +1,101 @@
+import config from '../config'
+import React from 'react'
+import emitter from '../emitter'
+
+export default React.createClass({
+
+  getInitialState() {
+    return {
+      walkable: []
+    }
+  },
+
+  componentDidMount() {
+    window.addEventListener('click', this.createPolygon)
+  },
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.createPolygon)
+  },
+
+  createPolygon(e) {
+    const s = this.state.walkable.concat([[e.clientX, e.clientY]])
+    this.setState({
+      walkable: s
+    })
+  },
+
+  render() {
+    const editorScale = 0.3
+    const bgScale = this.props.data.bg.scale
+    const bgURL = this.props.data.assets[this.props.data.bg.asset]
+    return <div>
+      <img style={{
+        height: `${config.size.y*editorScale}`,
+        margin: '0 auto',
+        opacity: 0.7,
+        zoom: bgScale
+        // World
+      }} src={bgURL} />
+      <div style={{
+        // In camera sight
+        position: 'absolute',
+        backgroundImage: `url(${bgURL})`,
+        backgroundPosition: `${this.props.data.world.pos[0]}px ${this.props.data.world.pos[1]}px`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: bgScale * this.props.scene.background.texture.width,
+        left: -this.props.data.world.pos[0],
+        top: -this.props.data.world.pos[1],
+        width: config.size.x,
+        height: config.size.y,
+        zoom: editorScale
+      }} />
+      <input
+        min="0"
+        max={(this.props.scene.background.texture.width * this.props.data.bg.scale) - config.size.x}
+        onChange={this.handleWorldXChange}
+        type="range"
+        defaultValue={-this.props.data.world.pos[0]} />
+      <input
+        min="0"
+        max={(config.size.y * this.props.data.bg.scale) - config.size.y}
+        onChange={this.handleWorldYChange}
+        type="range"
+        defaultValue={-this.props.data.world.pos[1]} />
+      <input
+        min="0.1"
+        max="3"
+        onChange={this.handleBgScaleChange}
+        step="0.1"
+        type="range"
+        defaultValue={this.props.data.bg.scale} />
+    </div>
+  },
+
+  handleBgScaleChange(e) {
+    const d = this.props.data
+    d.bg.scale = e.target.value
+    // TODO make sure cords respect scale
+    emitter.emit('u_state', d)
+  },
+  handleWorldXChange(e) {
+    const d = this.props.data
+    d.world.pos[0] = -e.target.value
+    emitter.emit('u_state', d)
+  },
+  handleWorldYChange(e) {
+    const d = this.props.data
+    d.world.pos[1] = -e.target.value
+    emitter.emit('u_state', d)
+  }
+})
+
+    //<svg style={{ width: '100%', height: '100%'}}>
+      //{this.state.walkable.length ?
+        //<path style={{stroke: 'red', fill: 'red'}} d={`M${this.state.walkable[0][0]} ${this.state.walkable[0][1]}` + this.state.walkable.slice(1).map((d) => {
+            //return 'L' + d[0] + " " + d[1]
+          //}) + 'Z'}>
+        //</path>
+        //: null
+      //}
+    //</svg>
--- src/main.js
+++ src/main.js
@@ -1,87 +1,9 @@
-
 import createScene from './view/view_entry'
+import createEditor from './levdev/main'
+
 //import './levdev'
 import config from './config'
+import State from './state'
 
-createScene({
-  world: {
-    pos: [0, 0]
-  },
-  bg: {
-    width: config.size.x,
-    height: config.size.y,
-    asset: 'test',
-    scale: 1,
-    tiling: {
-      scale: 0.3
-    }
-  },
-  assets: {
-    test: './t.jpg', //'./test.jpg',
-    monsters: './monsters-32x32.png'
-  },
-  walkable: [
-    [199, 689], [2, 731], [3, 758],
-    [641, 788], [582, 700], [605, 617],
-    [844, 628], [829, 595], [516, 601],
-    [488, 645], [438, 694], [365, 749],
-    [206, 732] ],
-  walkbehind: {
-    bounds: [
-      [626, 612], [604, 608], [602, 590],
-      [590, 584], [588, 554], [556, 548],
-      [554, 512], [546, 506], [548, 500],
-      [554, 500], [558, 474], [836, 460],
-      [842, 630], [626, 680] ],
-    holes: [
-      [ [652, 538], [652, 520], [656, 516],
-        [672, 516], [670, 538]
-      ], [
-        [688, 538], [688, 516], [706, 514],
-        [706, 538]
-      ], [
-        [568, 536], [568, 516], [590, 514],
-        [588, 536]
-      ]
-    ]
-  },
-  mainChar: {
-    spritesheet: {
-      asset: 'monsters',
-      anim: {
-        down: {
-          startFrame: [0, 1],
-          width: 32,
-          height: 32,
-          frames: 3
-        },
-        right: {
-          startFrame: [3, 1],
-          width: 32,
-          height: 32,
-          frames: 3
-        },
-        up: {
-          startFrame: [6, 1],
-          width: 32,
-          height: 32,
-          frames: 3
-        },
-        left: {
-          startFrame: [9, 1],
-          width: 32,
-          height: 32,
-          frames: 3
-        },
-        idle: {
-          startFrame: [1,1],
-          width: 32,
-          height: 32,
-          frames: 1
-        }
-      }
-    },
-    pos: [150, 750],
-    scale: 3
-  }
-})
+State.subscribe(state => createEditor(state))
+
