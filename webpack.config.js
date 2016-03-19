e4b9d64cad07b06e80f9d023ce1fdfdb9ec26ed8
Johannes <johannes.wikner@gmail.com>
2016-03-20 00:09:06 +0100

    Support production builds

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,45 @@ const webpack = require('webpack')
 const DIST_PATH = path.join(__dirname, 'dist')
 const SRC_PATH = path.join(__dirname, 'src')
 
+const PROD = process.env.NODE_ENV === 'production'
+
+const opts = {
+  hmr: !PROD,
+  minify: PROD,
+  env: JSON.stringify(process.env.NODE_ENV),
+  dev: JSON.stringify(!PROD)
+}
 module.exports = {
   devtool: 'source-map',
   cache: 'true',
   entry: {
-    main: [
+    main: (!opts.hmr ? [] : [
       'webpack-dev-server/client?http://localhost:8080',
       'webpack/hot/dev-server',
-      `${SRC_PATH}/main.js`,
-    ],
-    levdev: [
+    ]).concat(`${SRC_PATH}/main.js`),
+    levdev: (!opts.hmr ? [] : [
       'webpack-dev-server/client?http://localhost:8080',
       'webpack/hot/dev-server',
-      `${SRC_PATH}/levdev/main.js`
-    ]
+    ]).concat( `${SRC_PATH}/levdev/main.js`)
   },
   output: {
     path: DIST_PATH,
     filename: '[name].js'
-    //publicPath: '/static/'
   },
   target: 'web',
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   plugins: [
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      __DEV__: opts.dev,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: opts.env
+      }
+    })
   ],
   node: {
     fs: 'empty'
